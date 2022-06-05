135d8c7fcb7105827d112a52b136734403d9c591
tuanmcwar <tuanntch1@gmail.com>
2022-06-05 15:48:50 +0700

    edit listSong

--- src/component/ListSong.js
+++ src/component/ListSong.js
@@ -15,8 +15,9 @@ function ListSong() {
   }, [song]);
   return (
     <div className="lg:col-span-2 col-start-1 col-end-7 h-screen w-full  overflow-y-scroll z-10 ">
-      <div className="relative bg-red-800">
-        <table className="table-auto  absolute top-0 m-auto  w-full  border-collapse bg-slate-900 ">
+      <div className="relative bg-slate-800-800">
+        <table className="table-auto  absolute top- m-auto  w-full  border-collapse bg-slate-900 ">
+          <br />
           <thead className="text-white h-12  z-10 bg-slate-900 lg:text-2xl text-xs">
             <tr className="">
               <th className=" sticky top-0 lg:text-2xl text-xs  text-center bg-slate-900">
@@ -24,24 +25,23 @@ function ListSong() {
               </th>
               <th className=" sticky top-0 bg-slate-900">Tên bài hát</th>
               <th className="  sticky top-0 bg-slate-900">Ca sĩ</th>
-              <th className=" sticky top-0 lg:pr-4 pr-3 bg-slate-900 text-xs">
+              <th className=" sticky top-0 lg:pr-4 pr-3 bg-slate-900 lg:text-2xl text-xs">
                 <i className="fa fa-download pl-4"></i>
               </th>
             </tr>
           </thead>
-          <tbody className="Bt1">
+          <tbody className="">
             {DataSongs.map((song, index) => (
               <tr
                 key={index}
-                className={` h-12 text-gray-200 bg-slate-900 hover:bg-slate-600 hover:cursor-pointer    text-center ${
-                  idSong === song.id &&
-                  "!bg-slate-600 text-pink-300   text-center "
+                className={` h-12 text-gray-200 bg-slate-900 hover:bg-slate-600 hover:cursor-pointer ${
+                  idSong === song.id && "bg-slate-600 text-pink-300 "
                 }`}
                 onClick={() => handlePlaySong(song.id)}
               >
                 <td
-                  className={`  text-center sm:text-base text-xs  !hover:bg-slate-600 hover:cursor-pointer  w-[52px] h-[52px] visible  ${
-                    idSong === song.name && "bg-slate-600   text-left"
+                  className={` text-center sm:text-base text-xs  hover:bg-slate-600 hover:cursor-pointer  w-[52px] h-[52px] visible  ${
+                    idSong === song.name && "bg-slate-600 "
                   }`}
                   onClick={() => handlePlaySong(song.id)}
                 >
@@ -53,7 +53,7 @@ function ListSong() {
                 <td className="text-center sm:text-base text-xs lg:text-xl ">
                   {song.author}
                 </td>
-                <td className="text-right lg:pr-5 pr-3 ">
+                <td className="text-center sm:text-base text-xs lg:text-xl">
                   <a href={song.url}>
                     <i className="fa fa-download"></i>
                   </a>
@@ -63,8 +63,9 @@ function ListSong() {
             ;
           </tbody>
         </table>
-        <table className="table-auto  absolute top-0 m-auto left-0 w-full  border-collapse bg-transparent  ">
-          <thead className="text-white h-12  z-10 bg-transparent lg:text-2xl text-xs">
+        <table className="table-auto  absolute top-[0.5px] m-auto  w-full  border-collapse   ">
+          <br />
+          <thead className="text-white h-12  z-10 bg-slate-900 lg:text-2xl text-xs">
             <tr className="">
               <th className=" sticky top-0 lg:text-2xl text-xs  text-center bg-slate-900">
                 STT
@@ -76,20 +77,19 @@ function ListSong() {
               </th>
             </tr>
           </thead>
-          <tbody className="Bt1">
+          <tbody className="">
             {DataSongs.map((song, index) => (
               <tr
                 key={index}
                 className={` h-12 text-gray-200 bg-slate-900 hover:bg-slate-600 hover:cursor-pointer   ${
                   idSong === song.id &&
-                  "!bg-transparent text-pink-300  invisible "
+                  "bg-transparent text-pink-300 invisible "
                 }`}
                 onClick={() => handlePlaySong(song.id)}
               >
                 <td
-                  className={`  text-center  !hover:bg-slate-600 hover:cursor-pointer  w-[52px] h-[52px]  ${
-                    idSong === song.name &&
-                    "bg-slate-600 invisible  text-center "
+                  className={` text-center sm:text-base text-xs hover:bg-slate-600 hover:cursor-pointer  w-[52px] h-[52px]    ${
+                    idSong === song.name && "bg-slate-600   "
                   }`}
                   onClick={(ab) => handlePlaySong(song.id)}
                 >
@@ -101,7 +101,7 @@ function ListSong() {
                 <td className="text-center sm:text-base text-xs lg:text-xl ">
                   {song.author}
                 </td>
-                <td className="text-right lg:pr-5 pr-3 ">
+                <td className="text-center sm:text-base text-xs lg:text-xl ">
                   <a href={song.url}>
                     <i className="fa fa-download"></i>
                   </a>
